df7d9c9d24f7a3f2927c31d3eb04fddad2487e4c
namewyf <wyf19892552132@163.com>
2024-12-04 12:10:12 +0800

    commander comands参数配置demo

--- bin/demo-commander.js
+++ bin/demo-commander.js
@@ -4,8 +4,15 @@ program.version('1.0.0');
 program.option('-n ', "输出名称")
 // 添加一个 -t 或 --type 选项，需要一个参数值
 program.option("-t --type <type>", "项目类型")
-// 解析命令行参数
-program.parse(process.argv);
 // 获取所有选项的值
 const options = program.opts();
 console.log("opts=>", options);
+
+program.command("create <app-name>")
+    .description("创建一个新项目")
+    .action((name) => {
+        console.log(`创建项目:${name}`);
+    })
+
+// 解析命令行参数
+program.parse(process.argv);
