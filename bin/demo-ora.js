e848e9c960092af05e026897564e3987fd318a72
namewyf <wyf19892552132@163.com>
2024-12-05 23:11:44 +0800

    ora使用demo

---
+++ bin/demo-ora.js
@@ -0,0 +1,11 @@
+import ora from 'ora';
+
+const spinner = ora('Loading unicorns').start();
+
+setTimeout(() => {
+    spinner.color = 'yellow';//修改颜色
+    spinner.text = 'Loading rainbows';//修改名字
+    setTimeout(() => {
+        spinner.stop();
+    }, 1000);
+}, 2000);
