4b7250b0bc57377be66dc17c79361f3d5a24c6c0
namewyf <wyf19892552132@163.com>
2024-12-06 16:51:54 +0800

    commander配置

--- bin/index.js
+++ bin/index.js
@@ -3,14 +3,29 @@
 let { program } = require("commander")
 let { promisify } = require("util")
 let asyncfiglet = promisify(require("figlet"))
+let chalk = require("chalk")
+// 日志打印函数
+const log = (content) => {
+    console.log(chalk.yellow(content))
+}
 
 program.version("1.0.0")
 
 program.option("-n --name <type>", "output name")
+// 打印logo
 async function printLogo() {
     let data = await asyncfiglet("v-cli")
-    console.log(data)
+    log(data)
 }
+
+program.command("create <app-name>")
+    .description("创建一个新项目")
+    .action(async (name) => {
+        await printLogo()
+        log("准备创建项目……")
+    })
+
+
 printLogo()
 program.parse(process.argv)
 
